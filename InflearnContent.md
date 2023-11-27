825899557fa70b08a124549378b7c8fac0f5c19f
bukwon <dnu05043@naver.com>
2023-11-27 21:45:51 +0900

    섹션2

---
+++ InflearnContent.md
@@ -0,0 +1,20 @@
+# Inflearn - SpringBoot
+
+## 스프링 웹
+ ### 1. Static Web
+
+    - html파일 형식 같은 정적인 페이지
+![img_2.png](img_2.png)
+
+ ### 2. MVC와 템플릿 엔진
+
+    - MVC란 모델 뷰 컨트롤러를 합친 뜻이다.
+    - 정적 웹과 다르게 viewResolver에서 처리 후 변환하여 처리
+![img_3.png](img_3.png)
+
+ ### 3. API
+
+    - 객체를 반환하는 방식
+    - Converter를 통해 Json 혹은 그 외의 파일로 반환
+
+![img_4.png](img_4.png)
--- src/main/java/com/example/hellospring/controller/helloController.java
+++ src/main/java/com/example/hellospring/controller/helloController.java
@@ -1,8 +1,12 @@
 package com.example.hellospring.controller;
 
+import com.example.hellospring.HelloSpringApplication;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
@@ -11,5 +15,37 @@ public class helloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    public class Hello {
+        public String getName() {
+            return Name;
+        }
+
+        public void setName(String name) {
+            Name = name;
+        }
+
+        private String Name;
     }
 }
