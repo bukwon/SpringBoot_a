bbc45152e0abf459fc00976ee86f080641b98ca7
bukwon <dnu05043@naver.com>
2023-11-27 15:06:45 +0900

    JPA Start

---
+++ src/main/java/com/example/sb231127_jpa/Sb231127JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb231127_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231127JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb231127JpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sb231127_jpa/Sb231127JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb231127_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231127JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
